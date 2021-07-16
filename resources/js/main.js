16e89c8148ae772e85cf45af0423702967637ccc
Zilya Gibatullina <zilya.gh@gmail.com>
2021-07-16 11:19:40 +0200

    change a to btn, change click function, design btn

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,22 +1,31 @@
+// var divs = ["lime", "earth-tech", "jal", "busena"];
+// var visibleDivId = null;
+// function divVisibility(divId) {
+//   if (visibleDivId === divId) {
+//     visibleDivId = null;
+//   } else {
+//     visibleDivId = divId;
+//   }
+//   hideNonVisibleDivs();
+// }
+// function hideNonVisibleDivs() {
+//   var i, divId, div;
+//   for (i = 0; i < divs.length; i++) {
+//     divId = divs[i];
+//     div = document.getElementById(divId);
+//     if (visibleDivId === divId) {
+//       div.style.display = "block";
+//     } else {
+//       div.style.display = "none";
+//     }
+//   }
+// }
+
 var divs = ["lime", "earth-tech", "jal", "busena"];
-var visibleDivId = null;
-function divVisibility(divId) {
-  if (visibleDivId === divId) {
-    visibleDivId = null;
-  } else {
-    visibleDivId = divId;
-  }
-  hideNonVisibleDivs();
-}
-function hideNonVisibleDivs() {
-  var i, divId, div;
-  for (i = 0; i < divs.length; i++) {
-    divId = divs[i];
-    div = document.getElementById(divId);
-    if (visibleDivId === divId) {
-      div.style.display = "block";
-    } else {
-      div.style.display = "none";
-    }
-  }
-}
+
+var toggle = document.getElementById("lime-btn");
+var content = document.getElementById("lime");
+
+toggle.addEventListener("click", function () {
+  content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+});
